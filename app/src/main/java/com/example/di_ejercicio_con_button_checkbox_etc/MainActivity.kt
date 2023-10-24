533393f15de30e92b311ff799a5ccc7466bbadfb
carlosmilenaquesada_portatil <carlosmilenaquesada@gmail.com>
2023-10-24 15:44:23 +0200

    ultimo

--- app/src/main/java/com/example/di_ejercicio_con_button_checkbox_etc/MainActivity.kt
+++ app/src/main/java/com/example/di_ejercicio_con_button_checkbox_etc/MainActivity.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.di_ejercicio_con_button_checkbox_etc.ui.theme.Di_ejercicio_con_button_checkbox_etcTheme
+//para las corrutinas
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
